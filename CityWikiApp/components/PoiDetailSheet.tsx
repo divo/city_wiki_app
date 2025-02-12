327153ffb6c4ccd3a18438300b18690b0392c043
--show-origin <divoapple@gmail.com>
2025-02-12 17:28:01 +0000

    Fix for PoiDetailSheet blocking views behind

--- CityWikiApp/components/PoiDetailSheet.tsx
+++ CityWikiApp/components/PoiDetailSheet.tsx
@@ -283,6 +283,8 @@ const styles = StyleSheet.create({
     bottom: 0,
     zIndex: 999,
     elevation: 999,
+    backgroundColor: 'transparent',
+    pointerEvents: 'box-none',
   },
   bottomSheet: {
     shadowColor: '#000',
@@ -293,6 +295,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 4,
     elevation: 5,
+    backgroundColor: 'white',
   },
   handle: {
     backgroundColor: '#DDDDDD',

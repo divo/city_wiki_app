c1790ba717e0c2293a9d17a5dd812e34884b3169
--show-origin <divoapple@gmail.com>
2025-02-18 15:42:34 +0000

    Remove dublin

--- CityWikiApp/types/city.ts
+++ CityWikiApp/types/city.ts
@@ -36,11 +36,5 @@ export const cities: City[] = [
     imageUrl: 'rome_cover.png',
     iap_id: 'com.halfspud.CityWikiApp.rome',
   },
-  {
-    id: 'County Dublin',
-    name: 'County Dublin',
-    country: 'Ireland',
-    imageUrl: 'county_dublin_cover.png',
-    iap_id: 'com.halfspud.CityWikiApp.dublin',
-  }
+  
 ]; 
